511c17acef2a5a0d417a467c95c16aa8e82206cc
mayumi <mayumi@asadigital.net>
2019-08-29 01:23:07 +0800

    EOF, ヒアドキュメント, ルートパラメータの練習

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'AmaArbaroController@index')->name('AmaArbaro.index'); //追加
+
+Route::get('hello/{msg}', function($msg) {
+
+$html = <<<EOF
+<html>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1{font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+<body>
+<h1>Hello</h1>
+<p>{$msg}</p>
+<p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+    return $html;
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
